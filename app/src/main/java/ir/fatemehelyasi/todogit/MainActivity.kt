f06c3c015a9be3d0faf482e5d8bc2244baf57ebb
PC <Fatemeh Elyasi>
2022-10-02 21:30:31 +0330

    Create navigation Graph.

---
+++ app/src/main/java/ir/fatemehelyasi/todogit/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package ir.fatemehelyasi.todogit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ir/fatemehelyasi/todogit/fragments/update/UpdateFragment.kt
@@ -0,0 +1,21 @@
+package ir.fatemehelyasi.todogit.fragments.update
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ir.fatemehelyasi.todogit.R
+
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+}
